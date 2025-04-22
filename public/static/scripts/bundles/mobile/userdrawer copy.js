f953d87ee366bb130c586f7fcf2c938ef5e93ed6
Rahul Yadav <rahulyadav2005@live.com>
2025-04-22 16:54:26 +0530

    new changes all

--- public/static/scripts/bundles/mobile/userdrawer copy.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('drawer-avatar').style.backgroundImage = 'url(/static/Image/AvatarOptions/' + sessionStorage.getItem('avatar') + '.png)';
-document.getElementById('drawer-profilename').innerText = 'Hello, '+sessionStorage.getItem('userName')+'!';
-
-
-function SetUserDrawerProfile(){
-    document.getElementById('drawer-avatar').style.backgroundImage = 'url(/static/Image/AvatarOptions/' + sessionStorage.getItem('avatar') + '.png)';
-    document.getElementById('drawer-profilename').innerText = 'Hello, '+sessionStorage.getItem('userName')+'!';
-}
